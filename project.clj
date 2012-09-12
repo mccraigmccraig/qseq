68db4968944516b478b77dda29be21e560cab09b
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2012-09-12 12:32:38 +0100

    test on clojure 1.3 and 1.4

--- project.clj
+++ project.clj
@@ -3,12 +3,15 @@
   :url "http://github.com/mccraigmccraig/qseq"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojars.mccraigmccraig/core.incubator "0.1.1-SNAPSHOT"]
+  :dependencies [[org.clojars.mccraigmccraig/core.incubator "0.1.1-SNAPSHOT"]
                  [org.clojure/java.jdbc "0.2.2"]
                  [clojureql "1.0.4"]
                  [korma "0.3.0-beta11"]]
   :profiles {:dev
-             {:dependencies [[midje "1.4.0" :exclusions [org.clojure/clojure]]]}}
+             {:dependencies [[midje "1.4.0" :exclusions [org.clojure/clojure]]]}
+             :1.3
+             {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.4
+             {:dependencies [[org.clojure/clojure "1.4.0"]]}}
   :min-lein-version "2.0.0"
   :plugins [[lein-midje "2.0.0-SNAPSHOT"]])

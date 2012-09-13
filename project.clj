ddf59c0be616ece13bb4810ff922d3dc295d7f73
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2012-09-13 13:12:42 +0100

    make Korma and ClojureQL dev-profile dependencies

--- project.clj
+++ project.clj
@@ -1,15 +1,16 @@
-(defproject qseq "0.2.1"
+(defproject qseq "0.2.2"
   :description "qseq: a lazy sequence for simply and efficiently consuming Korma and ClojureQL queries"
   :url "http://github.com/mccraigmccraig/qseq"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojars.mccraigmccraig/core.incubator "0.1.1-20111019.122151-1"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojars.mccraigmccraig/core.incubator "0.1.1-20111019.122151-1"]
                  [org.clojure/tools.logging "0.2.4"]
-                 [org.clojure/java.jdbc "0.2.2"]
-                 [clojureql "1.0.4"]
-                 [mccraigmccraig/korma "0.3.0-beta11"]]
+                 [org.clojure/java.jdbc "0.2.2"]]
   :profiles {:dev
-             {:dependencies [[midje "1.4.0" :exclusions [org.clojure/clojure]]]}
+             {:dependencies [[midje "1.4.0" :exclusions [org.clojure/clojure]]
+                             [clojureql "1.0.4"]
+                             [mccraigmccraig/korma "0.3.0-beta11"]]}
              :1.3
              {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4

a2e09641343831fafd7d9d9f2aec193aca0eb7ca
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2012-12-04 14:18:01 +0000

    update point release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject qseq "0.3.0"
+(defproject qseq "0.3.1"
   :description "qseq: a lazy sequence for simply and efficiently consuming Korma and ClojureQL queries"
   :url "http://github.com/mccraigmccraig/qseq"
   :license {:name "Eclipse Public License"
